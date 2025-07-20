24f7abde1347134b7b23a0ff1cda840017beb591
pavankumar0827 <yenumulashankarpavankumar@gmail.com>
2025-07-20 11:19:31 +0530

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -15,8 +15,8 @@ CREATE TABLE silver.crm_cust_info(
 	cst_key NVARCHAR(50),
 	cst_firstname VARCHAR(50),
 	cst_lastname VARCHAR(50),
-	cst_martial_status VARCHAR(5),
-	cst_gndr VARCHAR(5),
+	cst_martial_status VARCHAR(20),
+	cst_gndr VARCHAR(20),
 	cst_create_date DATE,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
@@ -27,12 +27,13 @@ IF OBJECT_ID('silver.crm_prd_info','U') IS NOT NULL
 
 CREATE TABLE silver.crm_prd_info(
 	prd_id INT,
+	cat_id NVARCHAR(50),
 	prd_key NVARCHAR(50),
 	prd_nm NVARCHAR(50),
 	prd_cost INT,
 	prd_line NVARCHAR(50),
-	prd_start_dt DATETIME,
-	prd_end_dt DATETIME,
+	prd_start_dt DATE,
+	prd_end_dt DATE,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 
@@ -43,9 +44,9 @@ CREATE TABLE silver.crm_sales_details(
 	sls_order_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
-	sls_order_dt INT,
-	sls_ship_dt INT,
-	sls_due_dt INT,
+	sls_order_dt DATE,
+	sls_ship_dt DATE,
+	sls_due_dt DATE,
 	sls_sales INT,
 	sls_quantity INT,
 	sls_price INT,
@@ -56,7 +57,7 @@ IF OBJECT_ID('silver.erp_cust_az12','U') IS NOT NULL
 	DROP TABLE silver.erp_cust_az12
 
 CREATE TABLE silver.erp_cust_az12(
-	cid NVARCHAR(50),
+	cid INT,
 	bdate DATE,
 	gen NVARCHAR(20),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
@@ -67,7 +68,7 @@ IF OBJECT_ID('silver.erp_loc_a101','U') IS NOT NULL
 
 
 CREATE TABLE silver.erp_loc_a101(
-	cid NVARCHAR(50),
+	cid INT,
 	cntry NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
